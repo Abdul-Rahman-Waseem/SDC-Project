a86b71261257c260f172037a899a1978e18ad3f8
Abdul-Rahman-Waseem <look4rahman@gmail.com>
2025-12-12 13:05:04 +0500

    First Commit: pushed project with all generated code files

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,76 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Admin {
+
+    /**
+     * Default constructor
+     */
+    public Admin() {
+    }
+
+    /**
+     * asim zohaib
+     * adsfwdgh
+     */
+    private int adminId;
+
+    /**
+     * 
+     */
+    private String username;
+
+    /**
+     eqqeqeqw* grrtrd
+     */
+    private String password;
+
+
+
+    /**
+     * 
+     */
+    public void login() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void addRoom() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void updateRoom() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void removeRoom() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void viewReports() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void manageBookings() {
+        // TODO implement here
+    }
+
+}
---
+++ src/main/java/model/Report.java
@@ -0,0 +1,39 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Report {
+
+    /**
+     * Default constructor
+     */
+    public Report() {
+    }
+
+    /**
+     * 
+     */
+    private int reportId;
+
+    /**
+     * 
+     */
+    private String reportType;
+
+    /**
+     * 
+     */
+    private Date generatedDate;
+
+
+    /**
+     * 
+     */
+    public void generateReport() {
+        // TODO implement here
+    }
+
+}
